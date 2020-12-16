2e5572aced52801d651e05e6d81160f9de259ab7
ParadoxPod <paradoxpod@gmail.com>
2020-12-15 18:55:32 -0800

    NOW WE JUMPIN BOIIIIIIII
    GOT THOSE HOPS GOIN
    LOOK AT EM GO
    HES HOPPIN
    BOING BOING BOING
    THATS ONE HOPPI BOI

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,21 @@
 extends KinematicBody2D
 
 export var speed := 100
-export var gravity := 100
+export var jump := -200
+export var gravity := 500
+var direction := Vector2()
+var jumping := false
 
 func _physics_process(delta):
-	var direction := Vector2()
-	direction.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
-	direction.y = $gravity * 1
-
+	direction.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
+	
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+		jumping = true
+		direction.y = jump
+	direction.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	
 	if direction.x > 0:
 		$player_sprite.playing = true
 		$player_sprite.flip_h = true
@@ -17,4 +25,4 @@ func _physics_process(delta):
 	elif direction.x == 0:
 		$player_sprite.playing = false
 
-	move_and_collide(speed * direction * delta)
+	move_and_slide(direction, Vector2(0, -1))
